96e0ef216d6a80261d6f05c5ac70e0b7376faa9f
Adam Hartman <adamhartman5@gmail.com>
2021-12-19 02:11:58 -0500

    Create textbox and button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { useState, useEffect } from 'react'
 import axios from './axios';
 
 function App() {
+  const [city, setCity] = useState('')
   return (
     <div className="App">
-      
+      <input 
+        placeholder='City name' 
+        value={city} 
+        onChange={event => setCity(event.target.value)} 
+        />
+      <button type="submit">Get Weather</button>
     </div>
   );
 }
